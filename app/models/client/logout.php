d31549a9093d3f850971fa8a4396e455b22cc057
takuya084 <takuya084@gmail.com>
2021-10-19 09:05:02 +0900

    logout機能

---
+++ app/models/client/logout.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(dirname(__FILE__).'/../../../functions/require.php');
+
+session_start();
+
+$pdo = connectDb();
+
+if (isset($_COOKIE['BLOG'])) {
+	$auto_login_key = $_COOKIE['BLOG'];
+	setcookie('BLOG', '', time()-86400, '/');
+
+	$sql = "delete from client_auto_login where c_key = :c_key";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute(array(":c_key" => $auto_login_key));
+}
+//セッション内のデータ削除
+$_SESSION = array();
+
+//クッキーの無効化
+if (isset($_COOKIE[session_name()])) {
+	setcookie(session_name(), '', time()-86400, '/');
+}
+
+//セッションの破棄
+session_destroy();
+
+unset($pdo);
+
+header('Location:'.SITE_URL.'/login/');
+
+?>
